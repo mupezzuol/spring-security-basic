5c1ae04785c3d9b6dd61cbd3f2883497339a2a09
Murillo Pezzuol <murillopezzuol@hotmail.com>
2019-10-27 22:17:44 -0300

    Ajustando seguranca

---
+++ src/main/java/com/spring/teste/security/config/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.spring.teste.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	// 1. Autenticação
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("murillo").password("1234").roles("ADMIN")
+			.and()
+			.withUser("aux").password("aux").roles("AUX");
+	}
+	
+	
+	// 2. Aplicando a segurança em nossa aplicação 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+			.and()
+			.httpBasic()
+			.and()
+			.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	
+	// 3. Liberando recursos estáticos para serem liberados acesso
+	@Override
+ 	public void configure(WebSecurity web) {
+ 		//web.ignoring().antMatchers("/*.html");
+ 	}
+	
+	
+	// Desabilitando encoder da senha
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+}
--- src/main/java/com/spring/teste/security/resources/ProdutoController.java
+++ src/main/java/com/spring/teste/security/resources/ProdutoController.java
@@ -1,5 +1,6 @@
 package com.spring.teste.security.resources;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,7 @@ public class ProdutoController {
 	@ResponseBody
 	public String cadastrar() {
 		return "Produto cadastrado com sucesso.";
-	}	
+	}
 	
 	@PutMapping
 	@ResponseBody
@@ -32,10 +33,10 @@ public class ProdutoController {
 	}	
 	
 	@DeleteMapping("/{id}")
+	//@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@ResponseBody
 	public String deletar(@PathVariable int id) {
 		return "Produto de id "+id+ " foi deletado com sucesso.";
 	}	
-	
 	
 }
