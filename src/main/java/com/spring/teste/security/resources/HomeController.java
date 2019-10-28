f44dea7f0d76ce0cccf8bf8946dd7adcecd7eed3
Murillo Pezzuol <murillopezzuol@hotmail.com>
2019-10-27 21:16:37 -0300

    CRUD Produto

--- src/main/java/com/spring/teste/security/resources/HomeController.java
+++ src/main/java/com/spring/teste/security/resources/HomeController.java
@@ -1,5 +1,18 @@
 package com.spring.teste.security.resources;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
 public class HomeController {
 
+	@GetMapping()
+	@ResponseBody
+	public String helloWorld() {
+		return "Hello World!";
+	}	
+	
 }
---
+++ src/main/java/com/spring/teste/security/resources/ProdutoController.java
@@ -0,0 +1,41 @@
+package com.spring.teste.security.resources;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/produto")
+@RestController
+public class ProdutoController {
+
+	@GetMapping("/{id}")
+	@ResponseBody
+	public String listar(@PathVariable int id) {
+		return "Lista do produto de id "+id+" consultada com sucesso.";
+	}	
+	
+	@PostMapping
+	@ResponseBody
+	public String cadastrar() {
+		return "Produto cadastrado com sucesso.";
+	}	
+	
+	@PutMapping
+	@ResponseBody
+	public String atualizar() {
+		return "Produto atualizado com sucesso.";
+	}	
+	
+	@DeleteMapping("/{id}")
+	@ResponseBody
+	public String deletar(@PathVariable int id) {
+		return "Produto de id "+id+ " foi deletado com sucesso.";
+	}	
+	
+	
+}
